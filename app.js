5dc60e9bbb1cd4db4dab66da5053f150ee05be60
Glazov Yuri <glazov.y0@gmail.com>
2022-09-26 00:57:11 +0300

    move icons to better place

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.querySelector('.social-icons').onmousemove = e => {
 
     const x = e.pageX - e.target.offsetLeft;
     const y = e.pageY - e.target.offsetTop;
-    const deg = Math.atan2(y - 40, x - 40) / Math.PI * 180 + 90;
+    const deg = Math.atan2(y - 40 - 0.8 * window.innerHeight, x - 40 - 0.5 * window.innerWidth + 192) / Math.PI * 180 + 90;
     //const perc = Math.sqrt((y - 40)*(y - 40)+(x - 40)*(x - 40)) / (40 * Math.sqrt(2));
   
     e.target.style.setProperty('--deg', `${deg}deg`);
